0c15e0ada23c9f91d2a43c040736772994e0295b
volkov85 <xplz2007@gmail.com>
2019-05-24 11:50:24 +0600

    upload

---
+++ js/script.js
@@ -0,0 +1,1 @@
+var nav=document.querySelector(".main-nav"),toggler=document.querySelector(".header__toggle");document.addEventListener("DOMContentLoaded",function(){nav.classList.remove("main-nav--show"),toggler.classList.add("header__toggle--opened")}),toggler.addEventListener("click",function(e){nav.classList.toggle("main-nav--show"),toggler.classList.toggle("header__toggle--opened")});
